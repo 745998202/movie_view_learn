db138a0991ca3f616a5904970dd259a36aa21d3a
dragonyao <dragonyao@tencent.com>
2019-12-23 20:10:35 +0800

    baseline - installed express and build back project v0.2

---
+++ movie_view/src/components/NewHello.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hello ! {{myname}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'NewHello',
+  data () {
+    return {
+      myname: 'yaomenglong'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
