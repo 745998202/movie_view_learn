a10101e4006783e4c9ba6fee4363489f1415f0f2
dragonyao <dragonyao@tencent.com>
2019-12-25 20:00:25 +0800

    change users.js : add route /users/login

--- movie_view_back/book_service/routes/index.js
+++ movie_view_back/book_service/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+//路由引入
 var router = express.Router();
+//数据库引入
+var mongoose = require('mongoose');
+//定义路由
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/mongooseTest',function(req,res,next){
+  mongoose.connect('mongodb://localhost/pets',{useMongoClient:true});
+  mongoose.Promise = global.Promise;
+
+  var Cat = mongoose.model('Cat',{name : String});
+
+  var tom = new Cat({name : 'Tom'});
+  
+  tom.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log('success insert')
+    }
+  });
+  res.send('数据库连接测试')
 });
 
 module.exports = router;
